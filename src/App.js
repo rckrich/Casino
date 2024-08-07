e34f06f8d984c9d970288ca6b57726380210b54e
Dylan Ocampo <dylanomar@live.com.mx>
2024-08-07 12:54:13 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter, Routes } from "react-router-dom";
 import { EgyptPage } from "./pages/EgyptPage";
 import { DragonPage } from "./pages/DragonPage";
 import { Home } from "./pages/Home";
@@ -19,15 +19,18 @@ function App() {
     
     <div className="">
       
-      <Router>
+      <HashRouter>
 
         <Routes>
           <Route path= "/" element={<Home></Home>}/>
+          <Route path= "/Casino" element={<Home></Home>}/>
           <Route path= "/egypt" element={<EgyptPage></EgyptPage>}/>
+          <Route path= "#/egypt" element={<EgyptPage></EgyptPage>}/>
+          <Route path= "#egypt" element={<EgyptPage></EgyptPage>}/>
           <Route path= "/dragon" element={<EgyptPage></EgyptPage>}/>
 
         </Routes>
-      </Router>
+      </HashRouter>
       
     </div>
   );
--- src/pages/EgyptPage.js
+++ src/pages/EgyptPage.js
@@ -3,19 +3,14 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 
 export const EgyptPage = () => {
-
+  console.log("egypt");
     const { unityProvider } = useUnityContext({
-    loaderUrl: "Egypt/build/myunityapp.loader.js",
-    dataUrl: "Egypt/build/myunityapp.data",
-    frameworkUrl: "Egypt/build/myunityapp.framework.js",
-    codeUrl: "Egypt/build/myunityapp.wasm",
+    loaderUrl: "../UnityProjects/Egypt/build/myunityapp.loader.js",
+    dataUrl: "../UnityProjects/Egypt/build/myunityapp.data",
+    frameworkUrl: "../UnityProjects/Egypt/build/myunityapp.framework.js",
+    codeUrl: "../UnityProjects/Egypt/build/myunityapp.wasm",
   });
 
-    return (
-      
-    <div>
-      
-      <Unity unityProvider={unityProvider} />
-    </div>
-  );
+    return <Unity unityProvider={unityProvider} />
+  
 }
