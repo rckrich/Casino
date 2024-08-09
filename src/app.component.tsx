f82185d4fc77bc5423d68de7b093992297d1ca90
RodrigoCuevasCa <108311383+RodrigoCuevasCa@users.noreply.github.com>
2024-08-09 11:40:10 -0600

    casino fullscreen button

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,18 +1,20 @@
 import { useCallback, useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import styles from "./app.module.css";
+import Button from "react-bootstrap/Button";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
 
 const App = () => {
   const {
     unityProvider,
     isLoaded,
     loadingProgression,
-    sendMessage,
     addEventListener,
     removeEventListener,
     requestFullscreen,
-    takeScreenshot,
-    unload,
   } = useUnityContext({
     loaderUrl: "/unitybuild/CasinoBuild.loader.js",
     dataUrl: "/unitybuild/CasinoBuild.data",
@@ -24,16 +26,9 @@ const App = () => {
   });
 
   const [isPlaying, setIsPlaying] = useState(false);
-  const [screenshotDatas, setScreenshotDatas] = useState<string[]>([]);
   const [scores, setScores] = useState<[number, number][]>([]);
 
-  const handleClickStartGame = (time: number) => {
-    if (isLoaded === false || isPlaying === true) {
-      return;
-    }
-    setIsPlaying(true);
-    sendMessage("GameController", "StartGame", time);
-  };
+
 
   const handleClickFullscreen = () => {
     if (isLoaded === false) {
@@ -42,27 +37,6 @@ const App = () => {
     requestFullscreen(true);
   };
 
-  const handleClickScreenshot = () => {
-    if (isLoaded === false) {
-      return;
-    }
-    const screenshotData = takeScreenshot();
-    if (screenshotData !== undefined) {
-      setScreenshotDatas([screenshotData, ...screenshotDatas]);
-    }
-  };
-
-  const handleClickUnload = async () => {
-    if (isLoaded === false) {
-      return;
-    }
-    try {
-      await unload();
-      console.log("Unload success");
-    } catch (error) {
-      console.error(`Unable to unload: ${error}`);
-    }
-  };
 
   const handleGameOver = useCallback(
     (time: number, score: number) => {
@@ -82,7 +56,28 @@ const App = () => {
 
   return (
     <div className={styles.container}>
-      <h1>RCK</h1>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <h1 style={{font: "kanit"}}>Casino Demo</h1>
+      <div className="buttons" >
+        <Button href="#" 
+          onClick={handleClickFullscreen}
+          variant="primary"
+          style={{ marginLeft: "450%"
+          }}
+        >Fullscreen</Button>
+      </div>
       <div className={styles.unityWrapper}>
         {isLoaded === false && (
           <div className={styles.loadingBar}>
@@ -94,33 +89,9 @@ const App = () => {
         )}
         <Unity
           unityProvider={unityProvider}
-          style={{ display: isLoaded ? "block" : "none" }}
+          style={{ display: isLoaded ? "block" : "none", 
+          }}
         />
-      </div>
-      <div className="buttons">
-        <button onClick={() => handleClickStartGame(5)}>
-          Start Short Game
-        </button>
-        <button onClick={() => handleClickStartGame(10)}>
-          Start Long Game
-        </button>
-        <button onClick={handleClickFullscreen}>Fullscreen</button>
-        <button onClick={handleClickScreenshot}>Screenshot</button>
-        <button onClick={handleClickUnload}>Unload</button>
-      </div>
-      <h2>Scores</h2>
-      <ul>
-        {scores.map(([time, score]) => (
-          <li key={time}>
-            {score} points with {time} seconds left!
-          </li>
-        ))}
-      </ul>
-      <h2>Screenshots</h2>
-      <div className={styles.screenshots}>
-        {screenshotDatas.map((data, index) => (
-          <img width={250} key={index} src={data} alt="Screenshot" />
-        ))}
       </div>
     </div>
   );
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { createRoot } from "react-dom/client";
 import { createElement } from "react";
 import { App } from "./app.component";
 import "./styles.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./font.css";
 const element = document.getElementById("root")!;
 const root = createRoot(element);
 
